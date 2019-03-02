a066cf19b52acfc968f4629910cd2ff600273f58
James Dewes <james-dewes@users.noreply.github.com>
2019-03-02 23:15:18 +0000

    Calendar (#21)
    * Added fullcalender https://github.com/maddhatter/laravel-fullcalendar
    
    * Added twitter card links to the individual events pages and added GNU closes #15
    
    * started to add event metadata

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.master')
+@section('meta')
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@hullsangels_wargaming">
+<meta name="twitter:creator" content="@hullsangels_wargaming">
+<meta name="twitter:text:title" content="{{$event->title}}">
+<meta name="twitter:description" content="{{$event->title}} from {{$event->start}}">
+<meta name="twitter:image" content="https://hullsangels.com/img/logo.png">
+@endsction
 @section('content')
-<div class="container">
+<div class="container" itemscope itemtype="https://schema.org/Event">
  <div class="row">
    <div class="col-md-12">
-       <h1 class="mt-5 stencil">{{$event->title}}</h1>
+       <h1 class="mt-5 stencil" itemprop="name">{{$event->title}}</h1>
        <p>Start
-          <span class='date'>{{$event->start}}</span>
+          <span class='date' itemprop="startDate">{{$event->start}}</span>
        </p>
        <p>End
-          <span class='date'>{{$event->start}}</span>
+          <span class='date' itemprop="startTime">{{$event->start}}</span>
        </p>
        <hr>
      </div>
  </div>
  <div class="row">
    <div class="col-md-8">
-       <p>{{$event->description}}</p>
+       <p itemprop="about">{{$event->description}}</p>
  </div>
 @endsection
