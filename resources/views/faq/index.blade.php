4f616e5f041a7ce4b80dd8c23d587359f3902a9d
James Dewes <james-dewes@users.noreply.github.com>
2019-04-17 23:36:46 +0100

    Re-styled the FAQ to have collapsed elements and then expand.

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -3,6 +3,11 @@
     <meta name="description" value="All of the FAQ for Hull's Angels club, what to do when you forget your kit, who is in charge and what do we do.">
     <meta name="keywords" value="hull's angels faq, wargames faq, hull's angels club faq, wargaming club faq">
     <title>Hull's Angels FAQs</title>
+    <script>
+      function fold(element){
+        $(element).next('dd').toggle();
+      }
+    </script>
 @endsection
 @section('title')
   FAQ Section
@@ -12,8 +17,8 @@
    <div class="col-md-8">
      <dl class="faq">
        @foreach ($faqs as $faq)
-       <dt>{{$faq->question}}</dt>
-       <dd>{{$faq->answer}}</dd>
+       <dt onclick="fold(this)">{{$faq->question}}</dt>
+       <dd class="hidden">{!!$faq->answer!!}</dd>
 
         @endforeach
      </div>
