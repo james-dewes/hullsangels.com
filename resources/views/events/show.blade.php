72ab8d27efa6749c07d85d2956b1dc75d4ed8005
james-dewes <james-dewes@users.noreply.github.com>
2019-04-18 17:04:08 +0100

    Minor style changes to the events show page

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -6,23 +6,24 @@
 <meta name="twitter:text:title" content="{{$event->title}}">
 <meta name="twitter:description" content="{{$event->title}} from {{$event->start}}">
 <meta name="twitter:image" content="https://hullsangels.com/img/logo.png">
-@endsction
+@endsection
+@section('title')
+{{$event->name}}
+@endsection
 @section('content')
 <div class="container" itemscope itemtype="https://schema.org/Event">
  <div class="row">
    <div class="col-md-12">
-       <h1 class="mt-5 stencil" itemprop="name">{{$event->name}}</h1>
-       <p>Start
-          <span class='date' itemprop="startDate">{{$event->start}}</span>
-       </p>
-       <p>End
-          <span class='date' itemprop="startTime">{{$event->start}}</span>
+       <p>
+         <span class='date' itemprop="startDate">{{$event->start}}</span>
+          To
+          <span class='date' itemprop="startTime">{{$event->end}}</span>
        </p>
        <hr>
      </div>
  </div>
  <div class="row">
-   <div class="col-md-8">
+   <div class="col-md-12">
        <p itemprop="about">{!!$event->description!!}</p>
  </div>
 @endsection
