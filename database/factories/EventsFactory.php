748549c06f4d63f04345a18656c923509da079d2
james-dewes <james-dewes@users.noreply.github.com>
2019-08-05 12:56:12 +0100

    Added seeders and factories

---
+++ database/factories/EventsFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Events::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/RoleplayFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Roleplay::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
