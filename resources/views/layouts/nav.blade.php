39e02c18fbbab03d160aabf30d8c226241118585
james-dewes <james-dewes@users.noreply.github.com>
2019-08-13 13:11:54 +0100

    Fixes bug with csrf and the version of larvel for search

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -47,7 +47,7 @@
     </ul>
     <form class="form-inline my-2 my-lg-0" action="/search/" method="POST">
       <input class="form-control mr-sm-2" type="text" name="q" placeholder="Search" aria-label="Search">
-      @csrf
+      {{ csrf_field() }}
       <button class="btn my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -7,6 +7,6 @@
 <input class="form-control mr-sm-2" type="text" id="searchTerm" name="q" placeholder="Search" aria-label="Search">
 <br>
 <p class="text-center"><button class="btn my-2 my-sm-0" type="submit">Search</button></p>
-@csrf
+{{ csrf_field() }}
 </form>
 @endsection
